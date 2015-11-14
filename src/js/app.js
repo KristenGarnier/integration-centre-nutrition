22025c02c98bb1ad8268212528029c40958868d2
KristenGarnier <garnier.kristen@icloud.com>
2015-11-14 14:43:36 +0100

    Mise en place des pages actives sur le wallop equipe Mise en place d'un wallop sur les témoignages

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,29 @@ var Wallop = require('Wallop');
 var WallopElement = document.querySelector('.Wallop');
 var slider = new Wallop(WallopElement);
 var first  = false;
+var lastTarget = '';
+var originClassName = 'col-sm-3 text-center slider-bar';
 
 document.querySelector('.bar-component').addEventListener('click', function(e){
         if(e.target != this){
             if(!first && e.target.dataset.slide != 0){
                 slider.goTo(e.target.dataset.slide);
                 first = true;
+                console.log(e.target.class);
+                this.querySelector('.active').className = originClassName;
+                e.target.className += ' active';
+                lastTarget = e.target;
             }
             else {
                 if(first){
+                    lastTarget.className = originClassName;
                     slider.goTo(e.target.dataset.slide);
+                    e.target.className += ' active';
+                    lastTarget = e.target;
                 }
             }
         }
 });
+
+var WallopElementTem = document.querySelector('.WallopTem');
+var temoignages = new Wallop(WallopElementTem);
